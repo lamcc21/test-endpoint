f292276c749292baa0d38cb6c437098cb10aeb7f
Lachlan McCrae <8554309+lachlan1m@users.noreply.github.com>
2023-04-13 16:32:15 +1200

    add test script

---
+++ index.js
@@ -0,0 +1,34 @@
+const axios = require('axios');
+
+const endpoint = 'https://api.subquery.network/sq/subquery/kepler-testnet';
+const query = `
+  query {
+    acceptedOffers (first: 5) {
+      nodes {
+        id
+        indexerId
+        offerId
+        serviceAgreementId
+      }
+    }
+    indexers (first: 5) {
+      nodes {
+        id
+        controller
+        metadataId
+        commission
+      }
+    }
+  }
+`;
+
+const promises = Array(10).fill().map(() =>
+  axios.post(endpoint, { query })
+    .then((response) => console.log(response.data))
+    .catch((error) => console.error(error))
+);
+
+Promise.all(promises)
+  .then(() => console.log('All requests completed successfully'))
+  .catch((error) => console.error(`Error sending requests: ${error}`));
+
